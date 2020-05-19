296497e49f7465acc07b7ca197ff084bb19251da
Wannisson Freitas Pinho <wannissonpinho@fgfcontadores.com.br>
2020-05-19 00:06:03 -0300

    Removendo componentes

--- app/components/index.js
+++ app/components/index.js
@@ -1,41 +1,23 @@
 
 import Avatar, { AvatarAddOn } from './Avatar';
-import Card from './Card';
-import CardHeader from './CardHeader';
-import Checkable from './Checkable';
-import CustomInput from './CustomInput';
-import Divider from './Divider';
-import EmptyLayout from './EmptyLayout';
 import ExtendedDropdown from './ExtendedDropdown';
-import FloatGrid from './FloatGrid';
-import HolderProvider from './HolderProvider';
 import IconWithBadge from './IconWithBadge';
 import InputGroupAddon from './InputGroupAddon';
 import Layout, {
-    withPageConfig,
-    setupPage,
-    PageConfigProvider,
     PageConfigConsumer
 } from './Layout';
 import Nav from './Nav';
 import Navbar from './Navbar';
-import NavbarThemeProvider from './NavbarThemeProvider';
-import NestedDropdown from './NestedDropdown';
-import OuterClick from './OuterClick';
-import PageLoader from './PageLoader';
-import Progress from './Progress';
 import Sidebar from './Sidebar';
 import SidebarMenu from './SidebarMenu';
 import SidebarTrigger from './SidebarTrigger';
-import StarRating from './StarRating';
+
 import {
-    ThemeClass,
     ThemeProvider,
     ThemeSelector,
-    ThemeConsumer,
 } from './Theme';
-import Tools from './Tools';
-import UncontrolledModal from './UncontrolledModal';
+
+
 import UncontrolledPopover from './UncontrolledPopover';
 
 
