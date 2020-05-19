3d89ba9600551a1096333d1222edc59a0fd3bedc
Wannisson Freitas Pinho <wannissonpinho@fgfcontadores.com.br>
2020-05-19 00:41:20 -0300

    Removendo componentes

--- app/routes/Dashboards/Projects/ProjectsDashboard.js
+++ app/routes/Dashboards/Projects/ProjectsDashboard.js
@@ -1,40 +1,8 @@
 import React from 'react';
-import faker from 'faker/locale/en_US';
-import { Link } from 'react-router-dom';
 import {
-    Container,
-    Row,
-    Card,
-    CardBody,
-    Badge,
-    Table,
-    CardTitle,
-    Button,
-    InputGroup,
-    InputGroupAddon,
-    Input,
-    ListGroup,
-    ListGroupItem,
-    Media,
-    Col
+    Container
 } from './../../../components';
 import { setupPage } from './../../../components/Layout/setupPage';
-
-import { HeaderMain } from "../../components/HeaderMain";
-
-import {
-    TasksMedia
-} from "../../components/ProjectsDashboards/TasksMedia";
-import {
-    TinyDonutChart
-} from "../../components/ProjectsDashboards/TinyDonutChart"
-import {
-    TinyDonutChartAllProjects
-} from "../../components/ProjectsDashboards/TinyDonutChartAllProjects"
-import {
-    TimelineMini
-} from "../../components/Timeline/TimelineMini"
-import { DraggableProjects } from './DraggableProjects';
 
 const ProjectsDashboard = () => (
     <Container>
